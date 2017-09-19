b8cb8dbe8ce1c8fad2bd8987c135642368ae59d8
telgi <32012598+telgi@users.noreply.github.com>
2017-09-19 19:52:43 +0100

    first iteration
    passes all 16 tests

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,1 +1,23 @@
+class Hamming
+  def self.compute(original, mutation)
+    # checks for strands with inequal lengths
+    unless original.length == mutation.length
+      raise ArgumentError
+    end
 
+    # iterates through strands with equal lengths
+    difference = 0
+    mutation_char = mutation.split("")
+    original.each_char.with_index(0) do |character, index|
+      unless mutation_char[index] == character.to_s
+        difference = difference + 1
+      end
+    end
+
+    difference
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
